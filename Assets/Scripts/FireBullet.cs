837b962491036ef1e0b352f50fb1f24bbe61b6a5
Jacob De Hoyos <dehoyos.jacob@gmail.com>
2022-03-15 21:23:13 -0500

    Begin working on player actions

---
+++ Assets/Scripts/FireBullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour
+{
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform bulletSpawn;
+    [SerializeField] PlayerMovement player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.H) && player != null)
+        {
+            FireGun();
+            Debug.Log("Fired the gun!");
+        }
+    }
+
+    private void FireGun()
+    {
+        Instantiate(bullet, bulletSpawn.position, transform.rotation);
+    }
+}
